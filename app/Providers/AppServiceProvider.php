70f5c160abb343f14f68e71f5e9e8d8f13bdaf13
zinky <yazidzinky98@gmail.com>
2025-12-14 17:53:07 +0700

    okkk

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('production')) {
-            \Illuminate\Support\Facades\URL::forceScheme('https');
-        }
+        //
     }
 }
